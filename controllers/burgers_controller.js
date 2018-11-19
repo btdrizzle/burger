dbab8eafc87afabd0329a40e7cfeb7ff58d46bf3
btdrizzle <briandrake05@gmail.com>
2018-11-18 19:37:58 -0600

    mostly works.  css issues.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,12 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/.js");
+const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    var hbsObject = {
+    const hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
@@ -17,20 +17,20 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+  burger.create(["burger_name"], [req.body.burger_name], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update(
+  burger.update(
     {
-      sleepy: req.body.sleepy
+      devoured: req.body.devoured
     },
     condition,
     function(result) {
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,8 @@
 const orm = require('../config/orm');
 
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
 
-var burger = {
+const burger = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
